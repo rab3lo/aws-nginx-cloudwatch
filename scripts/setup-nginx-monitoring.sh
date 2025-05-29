d9c354af6a03d94d8125063eaa6b23d4e30e9e3c
Rafael Rabelo <115431009+rab3lo@users.noreply.github.com>
2025-05-29 18:45:56 -0300

    Update setup-nginx-monitoring.sh

--- scripts/setup-nginx-monitoring.sh
+++ scripts/setup-nginx-monitoring.sh
@@ -12,5 +12,22 @@ sudo systemctl start nginx
 # Habilita o Nginx para iniciar automaticamente
 sudo systemctl enable nginx
 
+# Instala o CloudWatch Agent
+sudo yum install -y amazon-cloudwatch-agent
+
+# Cria o diretório de configuração
+sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
+
+# Copia o arquivo de configuração
+sudo cp amazon-cloudwatch-agent.yaml /opt/aws/amazon-cloudwatch-agent/etc/
+
+# Inicia o CloudWatch Agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+-a start \
+-m ec2 \
+-c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.yaml \
+-s
+
+
 # Salve com permissão de execução
 chmod +x setup-nginx-monitoring.sh
